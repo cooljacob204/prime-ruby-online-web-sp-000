11f39bcdadf72536199a79af0e00ed5bb8e39d34
Jacob Feldman <jacobgfeld@gmail.com>
2018-10-21 18:31:20 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@
 def prime?(number, current = number - 1)
   return TRUE if number == 2
   return FALSE if number <= 1
-  return TRUE if 
+  return TRUE if current <= 1
   if number%current == 0
     return FALSE
   else
